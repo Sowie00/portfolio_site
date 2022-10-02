e32c369fe8fd1509207c20ce9a0425fe5ac26935
Sourav Pandya <pandyasourav38@gmail.com>
2022-10-02 04:35:39 -0400

    corrected UI errors

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,12 +1,7 @@
 import mongoose from "mongoose";
-require("dotenv").config();
 const connectMongo = async () => {
   try {
     const { connection } = mongoose.connect(process.env.MONGODB_URI);
-
-    if (connection.readyState == 1) {
-      console.log("Database Connected");
-    }
   } catch (err) {
     return Promise.reject(err);
   }
