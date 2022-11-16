32b3bc351403368d61471ab4591b7d6908c6f1cb
Sourav Pandya <pandyasourav38@gmail.com>
2022-11-16 03:42:11 -0500

    fixed screen constraints

--- constants/Data.js
+++ constants/Data.js
@@ -15,81 +15,33 @@ export const skillsData = [
     id: 2,
   },
   {
-    skillName: "HTML",
-    image: "/assets/skills/html.png",
-    id: 3,
-  },
-  {
     skillName: "Node.Js",
     image: "/assets/skills/node.png",
-    id: 4,
+    id: 3,
   },
   {
     skillName: "React",
     image: "/assets/skills/react.png",
-    id: 5,
+    id: 4,
   },
   {
     skillName: "Tailwind",
     image: "/assets/skills/tailwind.png",
-    id: 6,
+    id: 5,
   },
   {
     skillName: "Next.js",
     image: "/assets/skills/nextjs.png",
-    id: 7,
+    id: 6,
   },
   {
     skillName: "Mongo",
     image: "/assets/skills/mongo.png",
-    id: 8,
+    id: 7,
   },
   {
     skillName: "Github",
     image: "/assets/skills/github.png",
-    id: 9,
-  },
-];
-
-export const projects = [
-  {
-    title: "MERN Memories",
-    description:
-      "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-    image: "/assets/projects/crypto.jpg",
-    tags: ["Mongo", "Express", "React", "Node"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 0,
-  },
-  {
-    title: "E-Commerce",
-    description:
-      "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: "/assets/projects/netflix.jpg",
-    tags: ["React", "JavaScript"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 1,
-  },
-  {
-    title: "WebRTC App",
-    description:
-      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: "/assets/projects/property.jpg",
-    tags: ["React", "WebRTC"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 2,
-  },
-  {
-    title: "Unichat",
-    description:
-      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: "/assets/projects/twitch.jpg",
-    tags: ["React", "ChatEngine", "Firebase"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 3,
+    id: 8,
   },
 ];
